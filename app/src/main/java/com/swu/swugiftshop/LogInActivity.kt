079753b94d4e8ea75a0ec46c97b4ba0713f37f5e
revelvu <58437786+revelvu@users.noreply.github.com>
2020-08-05 14:47:10 +0900

    login
    로그인 화면 - 회원가입 화면

---
+++ app/src/main/java/com/swu/swugiftshop/LogInActivity.kt
@@ -0,0 +1,18 @@
+package com.swu.swugiftshop
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_log_in.*
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        registerBtn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
