8a21ddbea8192d4a6952253a0d77c151ddaf6173
revelvu <58437786+revelvu@users.noreply.github.com>
2020-08-09 19:11:10 +0900

    wishlist

--- app/src/main/java/com/swu/swugiftshop/WishlistFragment.kt
+++ app/src/main/java/com/swu/swugiftshop/WishlistFragment.kt
@@ -39,56 +39,56 @@ class WishlistFragment: Fragment() {
 }
 
 // Tab bar
-class TabPagerActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_wishlist)
-
-        tab_layout.addTab(tab_layout.newTab().setText("공식 굿즈"))
-        tab_layout.addTab(tab_layout.newTab().setText("비공식 굿즈"))
-
-        val pagerAdapter = FragmentPagerAdapter(supportFragmentManager, 2)
-        view_pager.adapter= pagerAdapter
-
-        tab_layout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                view_pager.setCurrentItem(tab!!.position)
-            }
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-        })
-        // pager가 이동 했을 때 tab bar를 이동시키는 코드
-        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
-    }
-}
-
-class FragmentPagerAdapter(
-    fragmentManager: FragmentManager,
-    val tabCount: Int
-): FragmentStatePagerAdapter(fragmentManager) {
-    override fun getItem(position: Int): Fragment {
-        when(position) {
-            0 -> {
-                return WishlistFragmentOne()
-            }
-            1 -> {
-                return WishlistFragmentTwo()
-            }
-
-            else -> return return WishlistFragmentOne()
-        }
-    }
-
-    override fun getCount(): Int {
-        return tabCount
-    }
-}
+//class TabPagerActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_wishlist)
+//
+//        tab_layout.addTab(tab_layout.newTab().setText("공식 굿즈"))
+//        tab_layout.addTab(tab_layout.newTab().setText("비공식 굿즈"))
+//
+//        val pagerAdapter = FragmentPagerAdapter(supportFragmentManager, 2)
+//        view_pager.adapter= pagerAdapter
+//
+//        tab_layout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                view_pager.setCurrentItem(tab!!.position)
+//            }
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//
+//
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//
+//            }
+//
+//        })
+//        // pager가 이동 했을 때 tab bar를 이동시키는 코드
+//        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+//    }
+//}
+//
+//class FragmentPagerAdapter(
+//    fragmentManager: FragmentManager,
+//    val tabCount: Int
+//): FragmentStatePagerAdapter(fragmentManager) {
+//    override fun getItem(position: Int): Fragment {
+//        when(position) {
+//            0 -> {
+//                return WishlistFragmentOne()
+//            }
+//            1 -> {
+//                return WishlistFragmentTwo()
+//            }
+//
+//            else -> return return WishlistFragmentOne()
+//        }
+//    }
+//
+//    override fun getCount(): Int {
+//        return tabCount
+//    }
+//}
 
