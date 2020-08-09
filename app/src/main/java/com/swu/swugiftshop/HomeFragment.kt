4f1caead991eee2f34787eb7cd2f053037dc8602
hyunj <hyunjung9934@naver.com>
2020-08-09 18:48:53 +0900

    상세페이지로 전환

--- app/src/main/java/com/swu/swugiftshop/HomeFragment.kt
+++ app/src/main/java/com/swu/swugiftshop/HomeFragment.kt
@@ -1,13 +1,18 @@
 package com.swu.swugiftshop
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment: Fragment() {
+
     companion object {
         const val TAG: String = "로그"
         fun newInstance(): HomeFragment {
@@ -15,8 +20,20 @@ class HomeFragment: Fragment() {
         }
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        detailImage1.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, DetailpageActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onAttach(context: Context) {
