a379a677d880ee3750e158fa30cbefb5ea462af6
slpjenny <68774371+slpjenny@users.noreply.github.com>
2020-08-11 23:28:15 +0900

    닉네임 커밋1-1

--- app/src/main/java/com/swu/swugiftshop/MainActivity.kt
+++ app/src/main/java/com/swu/swugiftshop/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
